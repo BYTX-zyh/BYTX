9f35d5dce44b4c42a0bf2c119b19df1a6f5ba3fa
BYTX-zyh <18235499556@163.com>
2020-02-19 22:01:13 +0800

    增加pwd

--- BYTX.cpp
+++ BYTX.cpp
@@ -1,20 +1,44 @@
+/**
+ * author:BYTX
+ * 
+ * 
+ * */
 #include <iostream>
+#include <direct.h>
 
 #define Test cout << "this is a test line\n"
 
 using namespace std;
+
+string get_pwd();
+
+
+string get_pwd()
+{
+	char char_get_pwd[256];
+    string s;
+	_getcwd(char_get_pwd, sizeof(char_get_pwd));
+	s=char_get_pwd;
+    return s;
+}
+
 void init()
 {
-    cout<<"dddd\n";
+    string _pwd=get_pwd();
+    cout<<_pwd<<endl;
+    // system("md _file");
 }
 int main(int argc, char *argv[])
 {
     string s = argv[1];
+    cout<<argc<<endl;
+    //init 无参数指令 初始化
     if (s == "init")
     {
         try
         {
-            if(argc!=2) throw "Error\n";
+            if(argc!=2) 
+                throw "Init Error,please input the right instructions.\nMaybe you want \"init\"?\n";
             init();
         }
         catch (char const * throw_string)
@@ -23,7 +47,7 @@ int main(int argc, char *argv[])
             exit(0);
         }
     }
-    cout<<"test"<<endl;
+    // cout<<"test"<<endl;
 
     return 0;
 }
